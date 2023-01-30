07a4e76f451f0953b2e4713de2b1dda1f9520dfc
Youssef KERROUM <78832703+YKerroum@users.noreply.github.com>
2023-01-30 22:52:36 +0100

    Modify trimmer decorator

--- trimmer_decorator.rb
+++ trimmer_decorator.rb
@@ -3,6 +3,6 @@ require_relative 'decorator'
 # class TrimmerDecorator
 class TrimmerDecorator < Decorator
   def correct_name
-    super.correct_name.strip
+    super.correct_name.length > 10 ? super.correct_name[0..9] : super.correct_name
   end
 end
