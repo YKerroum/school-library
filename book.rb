e93f08ca65b0e5f2fa8ec96163e348ba188ca70e
Youssef KERROUM <78832703+YKerroum@users.noreply.github.com>
2023-01-31 22:45:31 +0100

    Create the class Rental

--- book.rb
+++ book.rb
@@ -1,5 +1,7 @@
 # class Book
 class Book
+  attr_accessor :title, :author
+
   def initialize(title, author)
     @title = title
     @author = author
---
+++ rental.rb
@@ -0,0 +1,8 @@
+# class Rental
+class Rental
+  attr_accessor :date
+
+  def initialize(date)
+    @date = date
+  end
+end
